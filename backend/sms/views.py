6cf3aedae4b9a3535af9ab550e9a3f53d6ad2203
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-11-26 17:39:20 +0530

    Makes Inventory Page.

--- backend/sms/views.py
+++ backend/sms/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+def assigned(request):
+    return render(request, 'Assigned.html')
