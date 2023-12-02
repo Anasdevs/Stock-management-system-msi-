5677d830d1d0235706fd93372abf4411e637120e
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-12-02 21:34:30 +0530

    Modifies Database models

--- backend/sms/admin.py
+++ backend/sms/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Purchase, IssuedItem
+from .models import Purchase, IssuedItem, Items, Faculty
 
 admin.site.register(Purchase)
 admin.site.register(IssuedItem)
+admin.site.register(Items)
+admin.site.register(Faculty)
 
--- backend/sms/models.py
+++ backend/sms/models.py
@@ -8,19 +8,47 @@ DEPARTMENT_CHOICES = [
     ('B.ED', 'B.ED'),
 ]
 
+class Faculty(models.Model):
+    name = models.CharField(max_length=50)
+    faculty_id=models.IntegerField(unique=True)
+    department = models.CharField(max_length=50, choices=DEPARTMENT_CHOICES)
+
+    def __str__(self):
+        return (self.name,self.faculty_id,self.department)
+
+class Items(models.Model):
+    name = models.CharField(max_length=50)
+    Quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
 class Purchase(models.Model):
-    ItemName = models.CharField(max_length=50)
+    ItemName = models.ForeignKey(Items,max_length=50,on_delete=models.DO_NOTHING,related_name='purchase_itemname')
     Quantity = models.IntegerField()
+    Rate = models.IntegerField(default=0)
     Invoice_Number = models.IntegerField()
     Billing_Date = models.DateField()
     Date_of_Entry = models.DateField()
     Total_Amount = models.IntegerField()
     Seller_Name = models.CharField(max_length=50)
+    CGST = models.IntegerField(default=0)
+    SGST = models.IntegerField(default=0)
+    Extra_charges = models.IntegerField(default=0)
+    Invoice=models.FileField(upload_to='invoice',null=True,blank=True)
+
+    def __str__(self):
+        return (self.ItemName,self.Invoice_Number)
 
 class IssuedItem(models.Model):
-    ItemName = models.ForeignKey(Purchase, on_delete=models.CASCADE, related_name='issued_item_itemname') 
+    ItemName = models.ForeignKey(Purchase, on_delete=models.DO_NOTHING, related_name='issued_item_itemname') 
     Quantity = models.IntegerField()
-    Name_of_Employee = models.CharField(max_length=50)
+    Name_of_Employee = models.ForeignKey(Faculty,max_length=50,on_delete=models.DO_NOTHING,related_name='issued_item_name_of_employee')
     Issue_Date = models.DateField()
-    Department = models.CharField(max_length=50, choices=DEPARTMENT_CHOICES)
+    Department = models.ForeignKey(Faculty,max_length=50,choices=DEPARTMENT_CHOICES,on_delete=models.SET("Faculty Not Found"))
     Location = models.CharField(max_length=50)
+    physical_verification = models.BooleanField(default=False)
+
+    def __str__(self):
+        return (self.ItemName,self.Name_of_Employee,self.Department)
