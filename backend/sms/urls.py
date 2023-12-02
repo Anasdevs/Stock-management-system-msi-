9c24a441f4cec21acba41c39aa1406a13065ba31
Anas saifi <anassaifi1108@gmail.com>
2023-12-02 11:31:31 +0530

    added alert on submit

--- backend/sms/urls.py
+++ backend/sms/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('issued/', views.issued, name='issued'),
     path('sidebar/', views.sidebar, name='sidebar'),
     path('reports/', views.reports, name="reports"),
+    # path('new_entry/', views.new_entry, name='new_entry'),
 ]
--- backend/sms/views.py
+++ backend/sms/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import request
-from .forms import IssuedForm 
-from .models import IssuedItem
+from django.contrib import messages
+from .models import Purchase
 
 def index(request):
-    return render(request, 'home.html')
+    submitted = False
+
+    if request.method == 'POST':
+        item_name = request.POST.get('item_name')
+        quantity = request.POST.get('quantity')
+        invoice_number = request.POST.get('invoice_number')
+        billing_date = request.POST.get('billing_date')
+        date_of_entry = request.POST.get('date_of_entry')
+        total_amount = request.POST.get('total_amount')
+        seller_name = request.POST.get('seller_name')
+
+        Purchase.objects.create(
+            ItemName=item_name,
+            Quantity=quantity,
+            Invoice_Number=invoice_number,
+            Billing_Date=billing_date,
+            Date_of_Entry=date_of_entry,
+            Total_Amount=total_amount,
+            Seller_Name=seller_name
+        )
+
+        messages.success(request, 'Data submitted!')
+        submitted = True
+
+    return render(request, 'home.html', {'submitted': submitted})
+
 
 def issued(request):
     return render(request, 'Issued.html')
@@ -14,3 +39,6 @@ def sidebar(request):
 
 def reports(request):
     return render(request, 'reports.html')
+
+
+
