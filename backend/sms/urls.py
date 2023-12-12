b4fd8fc83d8fcc2758a350050526bfb1fe5c5aae
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-12-12 22:30:54 +0530

    Fixed Routes

--- backend/sms/urls.py
+++ backend/sms/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.dashboard, name="dashboard"),
+    path('purchase/', views.index, name='index'),
     path('issued/', views.issued, name='issued'),
-    path('reports/', views.reports, name="reports"),
-    path('dashboard/', views.dashboard, name="dashboard")
 ]
